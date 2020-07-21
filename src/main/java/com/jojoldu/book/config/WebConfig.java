2c4624eca58a1f11f02c10889a7fe3dd2be09248
tychejin1218 <tychejin@naver.com>
2020-07-21 17:11:07 +0900

    구글 로그인 연동

---
+++ src/main/java/com/jojoldu/book/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.jojoldu.book.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.jojoldu.book.config.auth.LoginUserArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(loginUserArgumentResolver);
+	}
+}
--- src/main/java/com/jojoldu/book/web/IndexController.java
+++ src/main/java/com/jojoldu/book/web/IndexController.java
@@ -1,12 +1,12 @@
 package com.jojoldu.book.web;
 
-import javax.persistence.PostRemove;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.jojoldu.book.config.auth.LoginUser;
+import com.jojoldu.book.config.auth.dto.SessionUser;
 import com.jojoldu.book.service.posts.PostsService;
 import com.jojoldu.book.web.dto.PostsResponseDto;
 
@@ -19,8 +19,14 @@ public class IndexController {
 	private final PostsService postsService;
 
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, @LoginUser SessionUser user) {
+		
 		model.addAttribute("posts", postsService.findAllDesc());
+
+		if (user != null) {
+			model.addAttribute("userName", user.getName());
+		}
+
 		return "index";
 	}
 
@@ -28,13 +34,13 @@ public class IndexController {
 	public String postsSave() {
 		return "posts-save";
 	}
-	
+
 	@GetMapping("/posts/update/{id}")
 	public String postsUpdate(@PathVariable Long id, Model model) {
-		
+
 		PostsResponseDto postsResponseDto = postsService.findById(id);
 		model.addAttribute("post", postsResponseDto);
-		
+
 		return "posts-update";
 	}
 }
